c0e5cc4779ea14911d63ad73d49c02e70264261b
Muganwa <12413798+muganwas@users.noreply.github.com>
2021-08-18 21:16:18 +0300

    api dev port default from 8080 to 5000

--- backend-api/server.js
+++ backend-api/server.js
@@ -7,7 +7,7 @@ const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
-const PORT = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : (process.env.PORT || 8080);
+const PORT = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : (process.env.PORT || 5000);
 
 const whitelist = [
     'http://localhost:3000',
